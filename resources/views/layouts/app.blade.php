eff2ba0f4ae4d203ff0490d7b4c180131c83173a
StéphanePittier <stefpittier@gmail.com>
2017-02-01 11:13:53 +0100

    include bootstrap and layout bases

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        @include('includes.head')
+    </head>
+    <body>
+       
+        <div class="container">
+        
+        </div>
+    </body>
+    <footer>
+        @include('includes.footer')
+    </footer>
+</html>
