90c5a43147dfc2d09e3090825eb0bd843d47f992
sachagrenier <sachag.1125@gmail.com>
2017-02-02 12:58:56 +0100

    Sources fixes

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
 <meta name="description" content="ui">
-<meta name="author" content="Sacha grenier & Stéphane Pittier">
+<meta name="author" content="Sacha Grenier & Stéphane Pittier">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,14 +5,25 @@
     @include('includes.head')       
 </head>
 
-<!-- Bootstrap Css -->
- {{ Html::style('css/bootstrap.min.css') }}
- {{ Html::style('css/bootstrap.css') }}
- {{ Html::style('css/styles.css') }}
+<!-- Styles -->
+{{ Html::style('css/bootstrap.min.css') }}
+{{ Html::style('css/styles.css') }}
+{{ Html::script('js/jquery.min.js') }}
 
-<!-- Scripts JS -->
- {{ Html::script('js/jquery.min.js') }}
- {{ Html::script('js/bootstrap.min.js') }}
+<!-- Script Bootstrap -->
+{{ Html::script('js/bootstrap.min.js') }}
+
+
+<!-- Scripts jQueryUI -->
+{{ Html::script('js/jquery.min.js') }}
+{{ Html::script('js/jquery-ui.js') }}
+{{ Html::style('css/jquery-ui.css') }}
+
+<!-- Scripts DynaTable -->
+{{ Html::script('js/jquery.dynatable.js') }}
+{{ Html::style('css/jquery.dynatable.css') }}
+
+
 
 <body>
 @include('includes.header')	
