7b75ff0981caa900b04e2c049afbf10735801b07
sachagrenier <sachag.1125@gmail.com>
2017-02-03 15:58:44 +0100

    ui

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,20 +8,20 @@
 	<!-- Styles -->
 	{{ Html::style('css/bootstrap.min.css') }}
 	{{ Html::style('css/styles.css') }}
-	{{ Html::script('js/jquery.min.js') }}
-
-	<!-- Script Bootstrap -->
-	{{ Html::script('js/bootstrap.min.js') }}
-
 
 	<!-- Scripts jQueryUI -->
 	{{ Html::script('js/jquery.min.js') }}
 	{{ Html::script('js/jquery-ui.js') }}
 	{{ Html::style('css/jquery-ui.css') }}
 
+		<!-- Script Bootstrap -->
+	{{ Html::script('js/bootstrap.min.js') }}
+
 	<!-- Scripts DynaTable -->
 	{{ Html::script('js/jquery.dynatable.js') }}
 	{{ Html::style('css/jquery.dynatable.css') }}
+
+
 
 
 
